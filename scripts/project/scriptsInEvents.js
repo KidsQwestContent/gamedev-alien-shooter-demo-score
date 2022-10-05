14d119117fa46b142b0c76b8fe3b95ad015a2609
Vina <azharmuhammadptic@gmail.com>
2022-10-05 16:27:22 +0800

    0.1

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,14 @@
+
+
+
+const scriptsInEvents = {
+
+	async E_result_Event2_Act3(runtime, localVars)
+	{
+		var score = runtime.globalVars.highscore
+	}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
